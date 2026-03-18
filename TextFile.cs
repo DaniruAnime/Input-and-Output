5687447b297c7d8b3483616a186948c264e6dfe6
DaniruAnimeshnik <animeshnikdaniru@gmail.com>
2026-03-18 18:24:35 +0700

    Сериализация и десериализация

--- TextFile.cs
+++ TextFile.cs
@@ -19,14 +19,39 @@ public class TextFile
 
     public void SaveXml(string path)
     {
-        XmlSerializer serializer = new XmlSerializer(this.GetType());
-        using (FileStream fs = new FileStream(path, FileMode.Create))
-        {
-            serializer.Serialize(fs, this);
-        }
+        XmlSerializer serializer = new XmlSerializer(typeof(TextFile));
+        FileStream fileStream = new FileStream(path, FileMode.Create);
+        
+        serializer.Serialize(fileStream, this);
+        fileStream.Close();
     }
 
+    public static TextFile LoadXml(string path)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(TextFile));
+        FileStream fileStream = new FileStream(path, FileMode.Open);
+        TextFile temp = (TextFile)serializer.Deserialize(fileStream);
 
+        fileStream.Close();
+        return temp;
+    }
 
+    public void SaveBinary(string path)
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        FileStream fileStream = new FileStream(path, FileMode.Create);
+        
+        binaryFormatter.Serialize(fileStream, this);
+        fileStream.Close();
+    }
 
+    public static TextFile LoadBinary(string path)
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        FileStream fileStream = new FileStream(path, FileMode.Open);
+        TextFile temp = (TextFile)binaryFormatter.Deserialize(fileStream);
+
+        fileStream.Close();
+        return temp;
+    }
 }
